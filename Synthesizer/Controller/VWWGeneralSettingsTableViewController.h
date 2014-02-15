5bbf16c551f7050b8b1e3a0da0f75ccf413a5ea0
Zakk Hoyt <zakk@threefoldphotos.com>
2014-02-14 17:07:10 -0800

    Added general view controller

---
+++ Synthesizer/Controller/VWWGeneralSettingsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  VWWGeneralSettingsTableViewController.h
+//  Synthesizer
+//
+//  Created by Zakk Hoyt on 2/14/14.
+//  Copyright (c) 2014 Zakk Hoyt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VWWGeneralSettingsTableViewController : UITableViewController
+
+@end
