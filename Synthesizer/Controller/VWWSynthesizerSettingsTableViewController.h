adff92d61602b3a00f836d533c009c5251acf2d6
Zakk Hoyt <zakk@threefoldphotos.com>
2014-02-14 16:58:14 -0800

    added synth config screens

---
+++ Synthesizer/Controller/VWWSynthesizerSettingsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  VWWSynthesizerSettingsTableViewController.h
+//  Synthesizer
+//
+//  Created by Zakk Hoyt on 2/14/14.
+//  Copyright (c) 2014 Zakk Hoyt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VWWSynthesizerSettingsTableViewController : UITableViewController
+
+@end
