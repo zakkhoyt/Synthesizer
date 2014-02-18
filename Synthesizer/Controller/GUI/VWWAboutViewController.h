5a7912df2834afc3ac104ad51700dfb8a730220a
Zakk Hoyt <zacharyhoyt@zakkimac.local>
2014-02-17 17:28:49 -0800

    Adopted black as a background color on most VCs

---
+++ Synthesizer/Controller/GUI/VWWAboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  VWWAboutViewController.h
+//  Synthesizer
+//
+//  Created by Zakk Hoyt on 2/17/14.
+//  Copyright (c) 2014 Zakk Hoyt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VWWAboutViewController : UIViewController
+
+@end
